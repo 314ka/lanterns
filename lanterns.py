dfb314cc5808097c03380f1e01408c900658bf9f
314ka <pasaguravlev123@gmail.com>
2024-12-29 14:54:18 +0300

    Доделал вроде

--- lanterns.py
+++ lanterns.py
@@ -5,5 +5,23 @@ symbols = ['R', 'B', 'Y', 'G']
 
 random_string = ''.join(random.choices(symbols, k=100))
 
-print(random_string)
+print("Случайная последовательность цветов:", random_string, sep=' ')
 
+lis_lis = []
+
+lis = []
+
+def find_symbols(n, lis, lis_lis):
+    if n in lis:
+        lis.clear()
+    else:
+        lis.append(n)
+        if len(lis) == 4:
+            lis_lis.append(lis[:])
+            lis.clear()
+         
+
+for i in random_string:
+    find_symbols(i, lis, lis_lis)
+
+print("Наборы последовательностей из 4 цветов:", lis_lis, sep=' ')
